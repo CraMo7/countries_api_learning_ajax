e0a489087d072d0c41dc1bbad6e044fc8fa9239a
Craig Morton <CraigMorton92@gmail.com>
2016-03-30 09:33:12 +0100

    refactor - stores dataset in localStorage to improve page load time

--- public/app.js
+++ public/app.js
@@ -1,49 +1,25 @@
 window.onload = function(){
   console.log('App started');
 
-  var url = "http://restcountries.eu/rest/v1";
-  var request = new XMLHttpRequest;
-  request.open("GET", url);
-
+  if (!countries){
+    console.log("local countries not found");
+    var url = "http://restcountries.eu/rest/v1";
+    var request = new XMLHttpRequest;
+    request.open("GET", url);
+    request.send();
   request.onload = function(){
     if (request.status === 200){
       // STUFF THAT NEEDS THE COUNTRIES DATA
       console.log("api GET successful");
       countries = JSON.parse(request.responseText);
-      
-      // gets previous country from localstorage:
-      index = JSON.parse(localStorage.getItem("last_country_index"));
-
-      for (var i = 0; i < countries.length; i++){
-        var option = document.createElement("option");
-        option.value = i;
-        option.innerText = countries[i].name;
-        dropDown.appendChild(option);
-      }
-      
-      dropDown.value = index;
-
-      showBordering(index);
-
-      updatePageDisplay(index);
-
-      dropDown.onchange = function(){
-        index = dropDown.value;
-        updatePageDisplay(index);
-
-        localStorage.setItem("last_country_index", JSON.stringify(index));
-      }
-
-
+      localStorage.setItem("countries_data", JSON.stringify(countries));
+      gotCountriesData();
       // console.log(getNameFromCode("GBR"));
-
-
-
-    } // if request status 200
+    }; // if request status 200 end
   }; // request.onload end
-  request.send();
-
-  //STUFF THAT NEEDS THE PAGE TO LOAD
+  } else {
+    gotCountriesData();
+  }; // if !countries end
   var mainSection = document.querySelector("section.main");
   mainSection.appendChild(dropDown);
 
@@ -52,9 +28,20 @@ window.onload = function(){
   mainSection.appendChild(capitalCityP);
   mainSection.appendChild(borderingP);
 
-}; // window.onload end
+  dropDown.onchange = function(){
+    index = dropDown.value;
+    updatePageDisplay(index);
+    localStorage.setItem("last_country_index", JSON.stringify(index));
+  }
 
+}; // window.onload end
+// //////////////////////////////////////////////////////////////////////////////////////////
 // STUFF THAT DOESN'T NEED WINDOW LOADED
+
+var countries = JSON.parse(localStorage.getItem("countries_data"));
+      // gets previous country from localstorage:
+var index = JSON.parse(localStorage.getItem("last_country_index"))
+
 var dropDown = document.createElement("select");
 var nameP = document.createElement("p");
 var populationP = document.createElement("p");
@@ -62,11 +49,19 @@ var capitalCityP = document.createElement("p");
 var borderingP = document.createElement("p");
 
 
-var showBordering = function(index){
-  borderCodes = countries[index].borders;
-  borderCountries = borderCodes.map(getNameFromCode);
-  return borderCountries;
-}
+var gotCountriesData = function() {
+
+  for (var i = 0; i < countries.length; i++){
+    var option = document.createElement("option");
+    option.value = i;
+    option.innerText = countries[i].name;
+    dropDown.appendChild(option);
+  }
+
+  dropDown.value = index;
+  getBorderingCountries(index);
+  updatePageDisplay(index);
+};
 
 var getNameFromCode = function(code){
   for (let i = 0; i < countries.length; i++){
@@ -74,14 +69,17 @@ var getNameFromCode = function(code){
       return countries[i].name;
     }
   }
-}
+};
+
+var getBorderingCountries = function(index){
+  borderCodes = countries[index].borders;
+  borderCountries = borderCodes.map(getNameFromCode);
+  return borderCountries;
+};
 
 var updatePageDisplay = function(index){
   nameP.innerText = "Country: " + countries[index].name;
   populationP.innerText = "Population: " + countries[index].population;
-  capitalCityP.innerText = "Capital City: " + countries[index].capital;
-  borderingP.innerText = "Bordering Countries: " + showBordering(index);
-}
-
-var index;
-var countries;
+  capitalCityP.innerText = "Capital City: " + countries[index].capital || "None";
+  borderingP.innerText = "Bordering Countries: " + (getBorderingCountries(index) || "No land borders");
+};
